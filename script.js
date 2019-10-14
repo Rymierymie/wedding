68eb280b4c9271288ff4d3acd04d7a2ceee30dd2
Ryman White <rymanwhite@gmail.com>
2019-10-14 22:02:43 +1100

    Heaps of updates.

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleRSVP() {
+    var x = document.getElementById("rsvp-form");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
